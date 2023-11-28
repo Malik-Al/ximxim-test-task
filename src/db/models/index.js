69c61c783808f8324553f2870878e7368d9f8cdc
malik <malikimenov@gmail.com>
2023-11-29 02:23:40 +0600

    Added to the file index.js tables, table links added

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -2,3 +2,10 @@ const { DB: sequelize } = require('../connection');
 const { Sequelize } = require('sequelize');
 
 const db = {};
+db.User = require('../models/user.model')(sequelize, Sequelize)
+db.File = require('../models/file.model')(sequelize, Sequelize)
+db.db.LogFile = require('../models/log.file.model')(sequelize, Sequelize)
+
+
+db.User.belongsToMany(File, { through: db.LogFile });
+db.File.belongsToMany(User, { through: db.LogFile });
