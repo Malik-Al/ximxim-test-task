91a389b89ba152399334b0b7b7131852eb426309
malik <malikimenov@gmail.com>
2023-11-30 14:21:17 +0600

    Changed database connections

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const cors = require('cors');
 const logger = require('./logger');
 const config = require('./msdata/config/main.json');
 const db = require('./src/db/connection');
+const router = require('./src/routes/index');
 const PORT = config.port;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/', router);
 
 const start = async () => {
     try {
-        await db.init();
+        db.sequelize.authenticate();
+        db.sequelize.sync();
         app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
     } catch (error) {
         throw Error(error);
