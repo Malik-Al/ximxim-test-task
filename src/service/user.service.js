69043b4567606f10e2f4d4f3deb1a02dab63fd39
malik <malikimenov@gmail.com>
2023-11-30 18:26:04 +0600

    Added to the registrationService method to receive the generated token and issue a response

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,11 +1,13 @@
 const { User } = require('../db/connection');
 const logger = require('../../logger');
 const bcrypt = require('bcrypt');
-
+const Token = require('./token.service');
 
 class UserService {
     async findOneService(id) {
-        logger.info('[START] findOneService Метода для поиска пользователя по полю id в базе')
+        logger.info(
+            '[START] findOneService Метода для поиска пользователя по полю id в базе',
+        );
         try {
             const result = await User.findAll({
                 where: { id: id },
@@ -13,31 +15,40 @@ class UserService {
             return result.map((el) => el.dataValues.id)[0] ? true : false;
         } catch (error) {
             console.log('error', error);
-            throw error
+            throw error;
         }
     }
 
-
     async registrationService(id, password) {
-        logger.info('[START] registrationService Метода для записи пользователя в базу')
+        logger.info(
+            '[START] registrationService Метода для записи пользователя в базу',
+        );
         try {
             const hashPassword = await bcrypt.hash(password, 5);
             const body = {
                 id,
                 password: hashPassword,
-                refresh_token: "refresh_token",
-                created_at: new Date()
-            }
-            const result = await User.create(body)
-            return result.dataValues.user_id
+                refresh_token: null,
+                created_at: new Date(),
+            };
 
+            const newUser = await User.create(body);
+
+            const user = {
+                user_id: newUser.dataValues.user_id,
+                id: newUser.dataValues.id,
+                password: newUser.dataValues.password,
+                created_at: newUser.dataValues.created_at
+            }
+
+            const tokens = Token.generate(user);
+            return tokens
+            
         } catch (error) {
             console.log('error', error);
-            throw error
+            throw error;
         }
     }
-
-
 }
 
 module.exports = new UserService();
