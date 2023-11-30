21ef5d9845444de1fd05d6c91ec1c3cb5d267bac
malik <malikimenov@gmail.com>
2023-11-30 21:31:11 +0600

    Added a router for issuing a new accessToken

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,7 +1,11 @@
 const Router = require('express');
 const { body } = require('express-validator');
 const router = new Router();
-const { registration, authorizations } = require('../controllers/user.controller');
+const {
+    registration,
+    authorizations,
+    generateNewAccessToken,
+} = require('../controllers/user.controller');
 
 router.post(
     '/signup',
@@ -15,5 +19,11 @@ router.post(
     body('id'),
     body('password').isLength({ min: 6, max: 30 }),
     authorizations,
+);
+
+router.post(
+    '/signin/new_token',
+    body('refreshToken').isLength({ min: 320, max: 400 }),
+    generateNewAccessToken,
 );
 module.exports = router;
