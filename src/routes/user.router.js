55a4e11cdd35d9d0a04a0ffeaa4e8e86238b4793
malik <malikimenov@gmail.com>
2023-11-30 20:13:27 +0600

    Added authorizations router

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,11 +1,19 @@
 const Router = require('express');
 const { body } = require('express-validator');
 const router = new Router();
-const { registration } = require('../controllers/user.controller');
+const { registration, authorizations } = require('../controllers/user.controller');
 
-router.post('/signup', 
-    body("id"), 
-    body("password").isLength({min: 6, max: 30}), 
-    registration);
+router.post(
+    '/signup',
+    body('id'),
+    body('password').isLength({ min: 6, max: 30 }),
+    registration,
+);
 
+router.post(
+    '/signin',
+    body('id'),
+    body('password').isLength({ min: 6, max: 30 }),
+    authorizations,
+);
 module.exports = router;
