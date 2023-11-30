2d2799e5596f9ddb5da69283e0e95da56d859bef
malik <malikimenov@gmail.com>
2023-11-30 14:25:23 +0600

    Changed the location of files

--- src/db/models/index.js
+++
@@ -1,9 +0,0 @@
-const { DB: sequelize } = require('../connection');
-const { Sequelize } = require('sequelize');
-
-const db = {};
-db.User = require('../models/user.model')(sequelize, Sequelize)
-db.File = require('../models/file.model')(sequelize, Sequelize)
-
-db.User.hasMany(db.File);
-db.File.belongsTo(db.User);
