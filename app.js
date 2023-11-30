598f06d1ceee1057be43bce7e7171d0e8a324eac
malik <malikimenov@gmail.com>
2023-11-30 16:17:55 +0600

    Added errorHandler

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const logger = require('./logger');
 const config = require('./msdata/config/main.json');
 const db = require('./src/db/connection');
 const router = require('./src/routes/index');
+const errorHandler = require('./src/middleware/error.handling')
 const PORT = config.port;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
