73239912736db7ef9f70a8fcfd53907c2aed0555
malik <malikimenov@gmail.com>
2023-12-01 14:42:54 +0600

    Added method uploadfile

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,14 +1,23 @@
 const { validationResult } = require('express-validator');
 const apiError = require('../error/api.error');
 const logger = require('../../logger');
+const FileService = require('../service/files.service');
 
 class FileController {
     async uploadfile(req, res, next) {
+        logger.info(
+            `[START] Запуск метода uploadfile для загрузки файла`,
+        );
         try {
-            res.status(200).json({
-                message: 'uploadfile',
+            const {file} = req.files
+            await FileService.uploader(file)
+            res.json({
+                message: "success"
             });
-        } catch (error) {}
+        } catch (error) {
+            console.error('Error uploadfile', error);
+            next(error);
+        }
     }
 }
 
