359f2e2fe19daf0cac682a233dea036e06ee7d33
malik <malikimenov@gmail.com>
2023-12-01 14:47:21 +0600

    Specified the path for static content

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const logger = require('./logger');
 const config = require('./msdata/config/main.json');
 const db = require('./src/db/connection');
 const router = require('./src/routes/index');
+const path = require('path')
+const fileUpload = require('express-fileupload');
 const errorHandler = require('./src/middleware/error.handling')
 const PORT = config.port;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'msdata/static')));
+app.use(fileUpload({}))
 app.use('/', router);
 
 app.use(errorHandler)
