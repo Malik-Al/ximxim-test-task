084f9ed8476f40958f8f7b41c9ba475aede3e3b8
malik <malikimenov@gmail.com>
2023-12-01 15:40:53 +0600

    Added method uploadfile

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -5,14 +5,16 @@ const FileService = require('../service/files.service');
 
 class FileController {
     async uploadfile(req, res, next) {
-        logger.info(
-            `[START] Запуск метода uploadfile для загрузки файла`,
-        );
+        logger.info(`[START] Запуск метода uploadfile для загрузки файла`);
         try {
-            const {file} = req.files
-            await FileService.uploader(file)
-            res.json({
-                message: "success"
+            const authorizationHeader = req.headers.authorization;
+            const accessToken = authorizationHeader.split(' ')[1];
+
+            const { file } = req.files;
+            await FileService.uploader(file, accessToken);
+
+            res.status(202).json({
+                message: 'File was successfully written',
             });
         } catch (error) {
             console.error('Error uploadfile', error);
